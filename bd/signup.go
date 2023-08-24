c9811af4e0bbeac858b5ac20139a507b346bb305
Renzo <renzmanri97@gmail.com>
2023-08-23 22:23:52 -0500

    Ultimo Commit

---
+++ bd/signup.go
@@ -0,0 +1,30 @@
+package bd
+
+import (
+	"fmt"
+
+	"github.com/renzoemh/gambituser/models"
+	"github.com/renzoemh/gambituser/tools"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySQL() + "')"
+	fmt.Println(sentencia)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("SignUp > Ejecucion exitosa")
+	return nil
+}
